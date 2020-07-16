fb5c9b7890e1ac7e6ef06997a8cc79e287503926
Riley-Bowyer <riley.d.bowyer@gmail.com>
2020-07-16 15:10:08 +1000

    Implemented all filters except extract indicies. Voxelise has been tested, all others have not.

--- include/lidar_cones_detection/PCLWrapper.hpp
+++ include/lidar_cones_detection/PCLWrapper.hpp
@@ -29,6 +29,12 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
 
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/conditional_removal.h>
+
 namespace uqr {
 
     /**
@@ -85,11 +91,148 @@ namespace uqr {
     //      also figure out if they're meant to be static or inline??
     //      and decide if they return a new cloud, or modify the existing cloud
     //          OR take a pointer to a second cloud as another parameter and load the new cloud into that??
-    static void voxelise();
-    static void pass_through_filter();
-    static void radius_outlier_removal();
-    static void sac_segmentation();
+
+    //      For input/output stuff, I'm a fan of leaving the input as be and modiyfing an output pointer.
+
+    static void voxelise(PointCloud &inputCloud, PointCloud &outputCloud, const double voxel_size){
+        // Convert to PCLPointCloud2 Format
+        pcl::PCLPointCloud2 pc2InputCloud(inputCloud);
+
+        // Define input and output clouds for filtering
+        pcl::PCLPointCloud2::Ptr preFilter(new pcl::PCLPointCloud2);
+        pcl::PCLPointCloud2 cloud_filtered;
+        
+        // Copy across data
+        pcl::copyPointCloud(pc2InputCloud, *preFilter);
+
+        // Define Input
+        pcl::VoxelGrid<pcl::PCLPointCloud2> filter;
+        filter.setInputCloud(preFilter);
+
+        // Configure Parameters
+        filter.setLeafSize(voxel_size, voxel_size, voxel_size);
+        
+        // Filter
+        filter.filter(cloud_filtered);
+
+        // Assign to output pointer
+        outputCloud = cloud_filtered;
+    }
+
+    static void pass_through_filter(PointCloud &inputCloud, PointCloud &outputCloud,
+                                    const std::string  field_name, const double lower_limit,
+                                    const double upper_limit, const bool invert_filter){
+        // Convert to PCLPointCloud2 Format
+        pcl::PCLPointCloud2 pc2InputCloud(inputCloud);
+
+        // Define input and output clouds for filtering
+        pcl::PCLPointCloud2::Ptr preFilter(new pcl::PCLPointCloud2);
+        pcl::PCLPointCloud2 cloud_filtered;
+        
+        // Copy across data
+        pcl::copyPointCloud(pc2InputCloud, *preFilter);
+        
+        // Define Input
+        pcl::PassThrough<pcl::PCLPointCloud2> filter;
+        filter.setInputCloud(preFilter);
+
+        // Configure Parameters
+        filter.setFilterFieldName(field_name);
+        filter.setFilterLimits(lower_limit, upper_limit);
+        filter.setFilterLimitsNegative(invert_filter);
+
+        // Filter
+        filter.filter(cloud_filtered);
+
+        outputCloud = cloud_filtered;
+    }
+    
+    static void radius_outlier_removal(PointCloud &inputCloud, PointCloud &outputCloud, 
+                                       const double radius, const double neighbours){
+        // Convert to PCLPointCloud2 Format
+        pcl::PCLPointCloud2 pc2InputCloud(inputCloud);
+
+        // Define input and output clouds for filtering
+        pcl::PCLPointCloud2::Ptr preFilter(new pcl::PCLPointCloud2);
+        pcl::PCLPointCloud2 cloud_filtered;
+        
+        // Copy across data
+        pcl::copyPointCloud(pc2InputCloud, *preFilter);
+        
+        // Define Input
+        pcl::RadiusOutlierRemoval<pcl::PCLPointCloud2> filter;
+        filter.setInputCloud(preFilter);
+
+        // Configure Parameters
+        filter.setRadiusSearch(radius);
+        filter.setMinNeighborsInRadius(neighbours);
+
+        // Filter
+        filter.filter(cloud_filtered);
+
+        outputCloud = cloud_filtered;
+    }
+
+    static void sac_segmentation(PointCloud &inputCloud, pcl::ModelCoefficients &outputCoeff, pcl::PointIndices &outputIndex,
+                                 const int model, const int method, const double threshold, const bool optimize_coeff){
+
+        // Method Types can be found at pcl/sample_consensus/method_types.h
+        // Model Types can be found at pcl/sample_consensus/model_types.h
+
+        // Convert to PCLPointCloud2 Format
+        pcl::PointCloud<pcl::PointXYZ> pclInputCloud(inputCloud);
+
+        // Define input clouds for filtering
+        pcl::PointCloud<pcl::PointXYZ>::Ptr preSegment(new pcl::PointCloud<pcl::PointXYZ>);
+
+        // Copy across data
+        pcl::copyPointCloud(pclInputCloud, *preSegment);
+        
+        // Define Input
+        pcl::SACSegmentation<pcl::PointXYZ> segment;
+        segment.setInputCloud(preSegment);
+
+        // Configure Parameters        
+        segment.setOptimizeCoefficients(optimize_coeff);
+        segment.setModelType(model);
+        segment.setMethodType(method);
+        segment.setDistanceThreshold(threshold);
+
+        // Segment
+        segment.segment(outputIndex, outputCoeff);
+    }
+
     static void extract_indices();
+
+    static void conditional_filter( PointCloud &inputCloud, PointCloud &outputCloud, 
+                                    pcl::ConditionAnd<pcl::PointXYZ>::Ptr condition){
+        // Sample condition: 
+        // pcl::ConditionAnd<pcl::PointXYZ>::Ptr range_cond (new pcl::ConditionAnd<pcl::PointXYZ> ());
+        // range_cond->addComparison(pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZ> ("z", pcl::ComparisonOps::GT, 0.0)));
+        // range_cond->addComparison(pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZ> ("z", pcl::ComparisonOps::LT, 0.8)));
+
+        // Convert to PCL::PointCloud Format
+        pcl::PointCloud<pcl::PointXYZ> pclInputCloud(inputCloud);
+
+        // Define input and output clouds for filtering
+        pcl::PointCloud<pcl::PointXYZ>::Ptr preFilter(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PointCloud<pcl::PointXYZ> cloud_filtered;
+        
+        // Copy across data
+        pcl::copyPointCloud(pclInputCloud, *preFilter);
+        
+        // Define Input
+        pcl::ConditionalRemoval<pcl::PointXYZ> filter;
+        filter.setInputCloud(preFilter);
+
+        // Configure Parameters
+        filter.setCondition(condition);
+
+        // Filter
+        filter.filter(cloud_filtered);
+
+        outputCloud = cloud_filtered;
+    }
 };
 
 #endif //LIDAR_CONES_DETECTION_PCLWRAPPER_H
--- src/test.cpp
+++ src/test.cpp
@@ -5,13 +5,63 @@
 
 
 #include "lidar_cones_detection/PCLWrapper.hpp"
+#include "pcl/io/pcd_io.h"
+#include "ros/ros.h"
 
+// Caleb Test Main Loop
+// int main(int argc, char** argv) {
+//     pcl::PCLPointCloud2::Ptr origin(new pcl::PCLPointCloud2());
+//     std::cout << "pcl::PCLPointCloud2::Ptr points to: " << origin << std::endl;
+//     uqr::PointCloud pointCloud(*origin);
+//     std::cout << "uqr::PointCloud points to: " << &pointCloud << std::endl;
+//     pcl::PCLPointCloud2 pclPointCloud = pointCloud;
+//     sensor_msgs::PointCloud2 sensorPointCloud = pointCloud;
+//     std::cout << "uqr::PointCloud points to: " << &pointCloud << std::endl;
+//     std::cout << "pcl::PCLPointCloud2 points to: " << &pclPointCloud << std::endl;
+//     std::cout << "sensor_msgs::PointCloud2 points to: " << &sensorPointCloud << std::endl;
+// }
+
+// Riley Test Main Loop
 int main(int argc, char** argv) {
-    pcl::PCLPointCloud2::Ptr origin(new pcl::PCLPointCloud2());
-    std::cout << "pcl::PCLPointCloud2::Ptr points to: " << origin << std::endl;
-    uqr::PointCloud pointCloud(*origin);
-    std::cout << "uqr::PointCloud points to: " << &pointCloud << std::endl;
-    pcl::PCLPointCloud2 pclPointCloud = pointCloud;
-    std::cout << "uqr::PointCloud points to: " << &pointCloud << std::endl;
-    std::cout << "pcl::PCLPointCloud2 points to: " << &pclPointCloud << std::endl;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> ("test_pcd.pcd", *cloud) == -1) //* load the file
+    {
+        PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+        return (-1);
+    }
+    std::cout << "Loaded "
+                << cloud->width * cloud->height
+                << " data points from test_pcd.pcd with the following fields: "
+                << std::endl;
+
+    uqr::PointCloud pointCloud(*cloud);
+    uqr::PointCloud pointCloudOut;
+    uqr::voxelise(pointCloud, pointCloudOut, 0.5);
+    
+    sensor_msgs::PointCloud2 inputCloud = pointCloud;
+    sensor_msgs::PointCloud2 outputCloud = pointCloudOut;
+
+    ros::init (argc, argv, "pub_pcl");
+    ros::NodeHandle nh;
+    ros::Publisher inputPub = nh.advertise<sensor_msgs::PointCloud2> ("input_cloud", 1);
+    ros::Publisher outputPub = nh.advertise<sensor_msgs::PointCloud2> ("output_cloud", 1);
+
+    inputCloud.header.frame_id = "map";
+    inputCloud.header.stamp = ros::Time::now();
+    outputCloud.header.frame_id = "map";
+    outputCloud.header.stamp = ros::Time::now();
+
+    ros::Rate loop_rate(4);
+    while (nh.ok())
+    {
+        inputCloud.header.frame_id = "map";
+        inputCloud.header.stamp = ros::Time::now();
+        outputCloud.header.frame_id = "map";
+        outputCloud.header.stamp = ros::Time::now();
+        inputPub.publish(inputCloud);
+        outputPub.publish(outputCloud);
+        ros::spinOnce ();
+        loop_rate.sleep ();
+    }
 }
